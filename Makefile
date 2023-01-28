264b48e0f564119a98a7e34ac44839b045a7c35d
AdamOsses <adamosses@users.noreply.github.com>
2023-01-28 03:44:17 +0100

    Dockerfile

--- Makefile
+++ Makefile
@@ -2,7 +2,7 @@ deps:
 	pip install -r requirements.txt
 
 lint:
-	flake8 simple_file.py
+	flake8 simple_file.py tests/test_w3schools.py
 
 run:
 	python3 simple_file.py
--- tests/test_w3schools.py
+++ tests/test_w3schools.py
@@ -1,13 +1,13 @@
 import unittest
 from selenium import webdriver
 from selenium.webdriver import FirefoxOptions
-from time import sleep
 
 
 class W3SchoolsTests(unittest.TestCase):
     def setUp(self):
         ff_options = FirefoxOptions()
-        ff_options.add_argument('--headless')     # It shut off display: Docker containers have no display.
+        ff_options.add_argument('--headless')
+    # It shut off display: Docker containers have no display.
         self.driver = webdriver.Firefox(options=ff_options)
         self.driver.get("https://www.w3schools.com/python/")
 
@@ -22,5 +22,3 @@ class W3SchoolsTests(unittest.TestCase):
 
 if __name__ == "__main__":
     unittest.main(verbosity=2)
-
-
